33a57a1c573bb83e9a2f98fae6f6eca99af30e9e
Arnaud Aliès <arnaudalies.py@gmail.com>
2025-09-15 14:00:48 +0200

    Update generator.go

--- pkg/generator/generator.go
+++ pkg/generator/generator.go
@@ -1,17 +1,59 @@
 package generator
 
-import "github.com/mouuff/GoSubAI/pkg/types"
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/mouuff/GoSubAI/pkg/brain"
+	"github.com/mouuff/GoSubAI/pkg/types"
+)
 
 type GenerationType int32
 
-const (
-	APPEND_GEN       GenerationType = 0
-	REPLACE_WITH_GEN GenerationType = 1
-)
-
 type SubtitleGenerator struct {
-	SubstitleData  *types.SubtitleData
-	Prompt         string
-	Model          string
-	GenerationType GenerationType
+	context       context.Context
+	Brain         *brain.OllamaBrain
+	SubstitleData *types.SubtitleData
+	PropertyName  string
+	Prompt        string
+	Model         string
+	Template      string
+	Debug         bool
+}
+
+func (g *SubtitleGenerator) Generate() (*types.SubtitleData, error) {
+	resultSubtitleData := &types.SubtitleData{
+		Entries: []types.SubtitleEntry{},
+	}
+
+	for _, entry := range g.SubstitleData.Entries {
+		prompt := strings.Replace(g.Prompt, "{TEXT}", entry.Text, 1)
+		response, err := g.Brain.GenerateString(g.context, g.PropertyName, prompt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		resultText := g.Template
+		resultText = strings.Replace(resultText, "{ORIGINAL_TEXT}", entry.Text, 1)
+		resultText = strings.Replace(resultText, "{GENERATED_TEXT}", response, 1)
+		resultText = strings.Replace(resultText, "{PROMPT}", prompt, 1) // For debugging
+
+		resultSubtitleData.Entries = append(resultSubtitleData.Entries, types.SubtitleEntry{
+			Index: entry.Index,
+			Start: entry.Start,
+			End:   entry.End,
+			Text:  resultText,
+		})
+
+		if g.Debug {
+			fmt.Printf("Index: %d\n", entry.Index)
+			fmt.Printf("Prompt: %s\n", prompt)
+			fmt.Printf("Response: %s\n", response)
+			fmt.Printf("ResultText: %s\n", resultText)
+		}
+	}
+
+	return g.SubstitleData, nil
 }
